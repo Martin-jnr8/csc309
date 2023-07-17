2b41b7f4fe14478ddc03052e212e32d5d0e9dad8
Martin-jnr8 <105743785+Martin-jnr8@users.noreply.github.com>
2023-07-17 17:15:28 +0100

    Update index.php

--- index.php
+++ index.php
@@ -10,6 +10,14 @@
     <table>
         <thead>
             <!-- Complete the table header row -->
+            <tr>
+               <th>SN</th>
+               <th>Firsname</th>
+               <th>Lastname</th>
+               <th>Email</th>
+               <th>Gender</th>
+               <th>Date of birth</th>
+            </tr>
         </thead>
 
         <tbody>
@@ -18,7 +26,19 @@
                 // Connect to the DB
                 // Select all records from users table
                 // Display all the selected records
-            ?>
+             ?>
+                    <?php
+                      $users = getAllUsers();
+                      foreach ($users as $user) {
+                    ?>
+                     <tr>
+                        <td><?php echo $user['1']; ?></td>
+                        <td><?php echo $user['Martin']; ?></td>
+                        <td><?php echo $user['Echi']; ?></td>
+                        <td><?php echo $user['echimartin2020@gmail.com']; ?></td>
+                        <td><?php echo $user['M']; ?></td>
+                        <td><?php echo $user['8 feb 2003']; ?></td>
+                     </tr>   
         </tbody>
     </table>
 </body>
